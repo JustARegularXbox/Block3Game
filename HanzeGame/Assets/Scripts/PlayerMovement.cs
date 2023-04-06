def1d3f6bcb4c9a2ea829236296f8d828700e3ca
JustARegularXbox <47003538+JustARegularXbox@users.noreply.github.com>
2023-04-06 10:41:13 +0300

    lhjknasdf

---
+++ HanzeGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    private float moveSpeed = 10.0f;
+    private float jumpForce = 20.0f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
